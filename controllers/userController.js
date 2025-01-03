fb8f3c2defdf57649a7bfbfe5fa6c07204a3cb3f
sameer bhatta <sameerbhatta37@gmail.com>
2025-01-03 16:31:24 +0545

    signIn and Forget Password end point done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,7 @@
 const User=require('../models/userModel')
+const Token=require('../models/tokenModel')
+const crypto=require('crypto')
+const jwt=require('jsonwebtoken') //authentication 
 
 exports.postUser=async(req,res)=>{
     let user= new User({
@@ -19,4 +22,62 @@ exports.postUser=async(req,res)=>{
             return res.status(400).json({error:'email must be unique'})
         }
     })
+}
+
+
+exports.signIn=async(req,res)=>{
+    const {email,password}=req.body
+
+    const user=await User.findOne({email})
+    if(!user){
+        return res.status(403).json({error:'something went wrong'})
+    }
+
+    if(!user.authenticate(password)){
+        return res.status(400).json({error:"email and password does not match"})
+    }
+
+   const token=jwt.sign({_id:user.id},process.env.JWT_SECRET)
+
+   //Store in the cookie
+   res.cookie('mycookie',token,{expire:Date.now()+9999999})
+
+   const {_id,userName}=user
+   return res.json({token, user:{_id,userName,email}})
+}
+
+//forget password
+
+exports.forgetPassword=async(req,res)=>{
+    const user=await User.findOne({email:req.body.email})
+    if(!user){
+        return res.status(400).json({error:"email does not match"})
+    }
+    let token= new Token({
+        userId:user._id,
+        token:crypto.randomBytes(16).toString('hex')
+
+    })
+    token=await token.save()
+    if(!token){
+        return res.status(400).json({error:'failed to create a token'})
+    }
+}
+
+exports.resetPassword=async (req,res) => {
+    let token=await Token.findOne({token:req.params.token})
+    if(!token){
+        return res.status(400).json({error:'invalid token or token may expired'})
+    }
+    //if token found
+    let user= await User.findOne({_id:token.userId})
+    if(!user){
+        return res.status(400).json({error:'unable to find the valid user '})
+    }
+    user.password=req.body.password //this req.body.password and not used hashed password is used from virtual field password
+    user=await user.save()
+    if(!user){
+        return res.status(400).json({error:'failed to reset password '})
+    }
+    res.json({msg:'password reset successfully'})
 }
